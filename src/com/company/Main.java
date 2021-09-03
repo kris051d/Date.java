f3aecb914fb75fb4070251b3f1a945dfcf2eceaa
Kristoffer Hansen <kris051d@edu.easj.dk>
2021-09-03 16:33:28 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String day = "Friday";
+        String date = "3rd";
+        String month = "September";
+        String year = "2021";
+        System.out.print(day);
+        System.out.print(", ");
+        System.out.print(month);
+        System.out.print(" ");
+        System.out.print(date);
+        System.out.print(", ");
+        System.out.print(year);
+        System.out.println(".");
+        System.out.print(day);
+        System.out.print(" ");
+        System.out.print(date);
+        System.out.print(" ");
+        System.out.print(month);
+        System.out.print(" ");
+        System.out.print(year);
+        System.out.print(".");
+    }
+}
